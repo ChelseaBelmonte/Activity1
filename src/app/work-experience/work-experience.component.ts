6d2626d7e24e94cf3dc7f644954cfcc405c820cb
ChelseaBelmonte <157207061+ChelseaBelmonte@users.noreply.github.com>
2024-01-22 22:16:14 +0800

    Update work-experience.component.ts

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-work-experience',
   templateUrl: './work-experience.component.html',
-  styleUrl: './work-experience.component.css'
+  styleUrl: '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 })
 export class WorkExperienceComponent {
 
