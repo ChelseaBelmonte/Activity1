e3a31c75d99c249c501091515d3c7a1f00b70c77
ChelseaBelmonte <157207061+ChelseaBelmonte@users.noreply.github.com>
2024-01-22 22:11:17 +0800

    Update contact-info.component.ts

--- src/app/contact-info/contact-info.component.ts
+++ src/app/contact-info/contact-info.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact-info',
   templateUrl: './contact-info.component.html',
-  styleUrl: './contact-info.component.css'
+  styleUrl: '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 })
 export class ContactInfoComponent {
 
